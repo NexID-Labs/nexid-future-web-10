fdbecb0254fc2bb7208d655b1ef5d563d2dcded9
Muhammad Ousman <ousmansohail786@gmail.com>
2025-07-23 01:52:49 +0500

    Update vite.config.ts gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/nexid-future-web-10/", // ✅ This line tells Vite your site is served from a sub-path
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
