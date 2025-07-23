ea258f36c95bcf3b339be3dcb5ab632551bd35a1
MuhammadOusman <ousmansohail786@gmail.com>
2025-07-23 15:38:23 +0500

    base change

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/nexid-future-web-10/", // For GitHub Pages deployment
+  base: "/", // For GitHub Pages deployment
   plugins: [react()],
   resolve: {
     alias: {
